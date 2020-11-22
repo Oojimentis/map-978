6f2f1b44a14af2e35d6d670bd791553a93c65be3
trev <trev@trev>
2020-11-21 23:49:23 -0500

    -G-AIRMET Linestings

--- WebContent/test.js
+++ WebContent/test.js
@@ -1,29 +1,34 @@
 
-var map = L.map('map').setView([ 40.355, -75.15], 8);
+var map = L.map('map').setView([ 40.355, -75.15], 4);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
  maxZoom: 19,
  attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
       }).addTo(map);
 
-      L.control.scale().addTo(map);
+	L.control.scale().addTo(map);
       
-   	 $.getJSON("region.geo",function(hoodData){
-   		function onEachFeature(feature,layer){
-   	  		      layer.bindPopup(feature.properties.Named);
-   	  		           }
-   	    var geojson = L.geoJson(hoodData, {
-   	        onEachFeature: onEachFeature
-   	      	  }).addTo(map);
-   	  	  		});  
+	$.getJSON("region.geo",function(hoodData){
+		function onEachFeature(feature,layer){
+			layer.bindPopup(feature.properties.Named);
+		}
+		var geojson = L.geoJson(hoodData, {
+			onEachFeature: onEachFeature
+		}).addTo(map);
+	});  
 
-   	  	 $.getJSON("geo.txt",function(data){
-   	    L.geoJson(data,{
-   	  		 pointToLayer: function(feature,latlng ){
-   	        var marker = L.marker(latlng)
-   	     marker.bindPopup(feature.properties.Location + '<br/>' + feature.properties.MSG);
-   	        return marker; 
-   	        }
-   	  		 })  .addTo(map);
-   	   });     
+	var myStyle = {
+			"color": "#ff7800",
+   		    "weight": 5,
+   		    "opacity": 0.65
+   		};
+ 	
+	$.getJSON("geo2.txt",function(data){
+		function onEachFeature(feature,layer){
+			layer.bindPopup(feature.properties.MSG + '<br/>' + feature.properties.Location);
+		}
+		var moo = L.geoJson(data,{
+			onEachFeature: onEachFeature
+		}).addTo(map);
+	});     
 
