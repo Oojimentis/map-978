26f88748bb2c9eb3b5972a5d0ac5fd8ed8b7b477
trev <Oojimentis>
2020-12-16 13:27:40 -0500

    Sidebar cleanup

--- WebContent/map978.js
+++ WebContent/map978.js
@@ -1,170 +1,197 @@
+function reloadFunc(obj){
+			  location.reload();
+	}
 
-var map = L.map('map').setView([ 40.855, -73.15], 5);
+var map = L.map('map').setView([36.0, -75.26], 5); 
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-}).addTo(map);
+var osm=new L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png',{ 
+		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);;
+var Esri_WorldImagery = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+		attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
+var Esri_WorldGrayCanvas = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+		attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+		maxZoom: 16
+});
+var OpenStreetMap_BlackAndWhite = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+		maxZoom: 18,
+		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+});
 
-L.control.scale().addTo(map);
-/*   
-	$.getJSON("airmet.geojson",function(hoodData){
-		function onEachFeature(feature,layer){
-			layer.bindPopup(feature.properties.RepNum);	
-		}
-		var geojson = L.geoJson(hoodData, { 
-			onEachFeature: onEachFeature
-		}).addTo(map);
-	});  
-*/
+function getColor(colf){
+	return 	colf >=	60000 ? '#d75b5b':
+			colf >=	50000 ? '#d7995b':
+			colf >=	43000 ? '#d7b85b':
+			colf >=	42000 ? '#d7d75b':
+			colf >=	40000 ? '#99d75b':
+			colf >=	32000 ? '#5bd77a':
+			colf >=	24000 ? '#5bd7b8':
+			colf >=	22000 ? '#5bd7d7':
+			colf >=	20000 ? '#5bb8d7':
+			colf >=	18000 ? '#d75bd7':
+			colf >=	16000 ? '#99ffe6':
+			colf >=	12000 ? '#99ffff':
+			colf >=	10000 ? '#99e6ff':
+			colf >=	9000  ? '#99ccff':
+			colf >=	8000  ? '#d75bb8':
+			colf >=	7000  ? '#9999ff':
+			colf >=	6000  ? '#b399ff':
+			colf >=	4000  ? '#cc99ff':
+			colf >=	1000  ? '#f0f075':
+			colf >=	0 	  ?	'#ff99ff':
+							'blue';
+}	
 
-// ** AIRMET  test
-L.realtime({url: 'http://localhost:8000/airmet.geojson',
-	crossOrigin: true, type: 'json'},  {interval: 3 * 1000,
+// G-AIRMET
+var gairmet = L.realtime({
+     	url: 'http://localhost:8000/gairmet.geojson',
+		crossOrigin: true, type: 'json'
+	}, {interval: 2 * 1000,
 		style: function(feature){
-// var fillColor = '#' +Math.floor(Math.random()*16777215).toString(16);
-			var fillColor;
-			var  alt = feature.properties.Alt;
-			if  ( alt > 50000 ) fillColor = "#0000FF";
-			else if ( alt > 30001 ) fillColor = "#FFFF00";
-      		else if ( alt > 20001 ) fillColor = "#FF00FF";
-      		else if ( alt > 16000 ) fillColor = "#32CD32";
-      		else if ( alt > 10000 ) fillColor = "#800000";
-      		else if ( alt > 7000 )  fillColor = "#008000";
-      		else if ( alt > 5000 )  fillColor = "#FF8C00";
-      		else if ( alt > 2000 )  fillColor = "#008080";
-      		else if ( alt > 999 )   fillColor = "#000080";
-      		else fillColor = "#000000";  // no data
-			return { color: "#999", weight: 1, fillColor: fillColor, fillOpacity: .2 };
+			kolor  = getColor(feature.properties.Alt);
+			return { color: '#5D8C8C', weight: 2, fillColor: kolor,opacity: 1.0,fillOpacity: 0.2};
 		},
-		getFeatureId: function(featureData){
-			return featureData.properties.RepNum;
+			getFeatureId: function(featureData){
+				return featureData.properties.RepNum;
 		},
-    
-		onEachFeature: function(feature,layer){
-			layer.bindPopup('AIRMET: Rep ID:' + feature.properties.RepNum
-					+ '<br>Altitude: ' + feature.properties.Alt	
-			)}}
-).addTo(map);
+ 			onEachFeature: function (feature, layer){
+				layer.bindTooltip('G-AIRMET: Alt '+ feature.properties.RepNum);
+	           	layer.on('click',function(e){
+					layer.setStyle({color: 'yellow',opacity: 0.8, fillColor: 'yellow', fillOpacity: 0.5});
+					$('#f11').html('G-AIRMET');
+					$('#f12').html(e.target.feature.properties.Alt);
+					$('#f13').html(e.target.feature.properties.RepNum);
+					$('#f14').html(e.target.feature.properties.Ob);
+					gairmet.stop();});
+	           layer.on('mouseout',function(e){
+					gairmet.start();})		
+			},
+				filter: function(feature, layer) {   
+					var n = document.getElementById("bgalt").value;
+					var nn = parseInt(n, 10);
+					return (feature.properties.Alt >= nn );
+				}
+		}).addTo(map);  //Note turned on to start map with Data, Checkbox has checked property.
 
-//** SIGMET
-L.realtime({url: 'http://localhost:8000/sigmet.geojson',
-	crossOrigin: true, type: 'json'},  {interval: 3 * 1000,
+// AIRMET
+var	airmet = L.realtime({
+     	url: 'http://localhost:8000/airmet.geojson',
+		crossOrigin: true, type: 'json'
+	}, {interval: 2 * 1020,
 		style: function(feature){
-// var fillColor = '#' +Math.floor(Math.random()*16777215).toString(16);
-			var fillColor;
-			var  alt = feature.properties.Alt;
-			if  ( alt > 50000 ) fillColor = "#0000FF";
-			else if ( alt > 30001 ) fillColor = "#FFFF00";
-      		else if ( alt > 20001 ) fillColor = "#FF00FF";
-      		else if ( alt > 16000 ) fillColor = "#32CD32";
-      		else if ( alt > 10000 ) fillColor = "#800000";
-      		else if ( alt > 7000 )  fillColor = "#008000";
-      		else if ( alt > 5000 )  fillColor = "#FF8C00";
-      		else if ( alt > 2000 )  fillColor = "#008080";
-      		else if ( alt > 999 )   fillColor = "#000080";
-      		else fillColor = "#000000";  // no data
-			return { color: "#999", weight: 1, fillColor: fillColor, fillOpacity: .2 };
+			kolor  = getColor(feature.properties.Alt);
+				return { color: '#00cccc', weight: 2, fillColor: kolor,opacity: 1.0,fillOpacity: 0.2};
 		},
-		getFeatureId: function(featureData){
-			return featureData.properties.RepNum;
+			getFeatureId: function(featureData){
+				return featureData.properties.RepNum;
 		},
-    
-		onEachFeature: function(feature,layer){
-			layer.bindPopup('SIGMET: Rep ID:' + feature.properties.RepNum
-					+ '<br>Altitude: ' + feature.properties.Alt	
-			)}}
-).addTo(map);
+			onEachFeature: function (feature, layer) 
+			{
+				layer.bindTooltip('AIRMET: Alt ' + feature.properties.Alt);
+	           	layer.on('click',function(e){
+					layer.setStyle({fillColor: 'yellow', fillOpacity: 0.5});
+					$('#f11').html('AIRMET');
+					$('#f12').html(e.target.feature.properties.Alt);
+					$('#f13').html(e.target.feature.properties.RepNum);
+					$('#f14').html('n/a');
+					airmet.stop();});
+	           layer.on('mouseout',function(e){
+					airmet.start();})		
+			},
+				filter: function(feature,layer) {   
+					var n = document.getElementById("agalt").value;
+					var nn = parseInt(n, 10);
+					return (feature.properties.Alt >= nn );
+				}
+		}).addTo(map);;   
 
-//** G-AIRMET
-L.realtime({url: 'http://localhost:8000/gairmet.geojson',
-	crossOrigin: true, type: 'json'},  {interval: 3 * 1000,
+// SIGMET
+var	sigmet = L.realtime({
+     	url: 'http://localhost:8000/sigmet.geojson',
+		crossOrigin: true, type: 'json'
+	}, {interval: 2 * 1030,
 		style: function(feature){
-// var fillColor = '#' +Math.floor(Math.random()*16777215).toString(16);
-			var fillColor;
-			var wgt=1 ;
-	 	var col;
-			var  alt = feature.properties.Alt;
-			if  ( alt > 50000 ) fillColor = "#0000FF";
-			else if ( alt > 30001 ) fillColor = "#FFFF00";
-      		else if ( alt > 20001 ) fillColor = "#FF00FF";
-      		else if ( alt > 15999 ) fillColor = "#32CD32";
-      		else if ( alt > 10001 ) fillColor = "#800000";
-      		else if ( alt > 7000 )  fillColor = "#008000";
-      		else if ( alt > 5000 )  fillColor = "#FF8C00";
-      		else if ( alt > 2000 )  fillColor = "#008080";
-      		else if ( alt > 999 )   fillColor = "#000080";
-      		else fillColor = "#000000";  // no data
-    	
-
-			if (feature.geometry.type == "LineString")  col = "#EE7C7C" , wgt = 2;     
-			else col = "#999";
-     		
-      		
-			return { color: col, weight: wgt, fillColor: fillColor, fillOpacity: .2 };
+			kolor  = getColor(feature.properties.Alt);
+				return { color: '#00cccc', weight: 2, fillColor: kolor,opacity: 1.0,fillOpacity: 0.2};
 		},
-		getFeatureId: function(featureData){
-			return featureData.properties.RepNum;
+			getFeatureId: function(featureData){
+				return featureData.properties.RepNum;
 		},
-    
-		onEachFeature: function(feature,layer){
-			layer.bindPopup('G-AIRMET: Rep ID:' + feature.properties.RepNum
-					+ '<br>Altitude: ' + feature.properties.Alt	
-					+ '<br>' + feature.properties.Ob
-	
-			)}}
-).addTo(map);
-
- 
-	var wxIcon = L.icon({iconUrl: 'therm.ico', iconSize: [35,25]});
+			onEachFeature: function (feature, layer) 
+			{
+				layer.bindTooltip('SIGMET: Alt ' + feature.properties.Alt);
+	           	layer.on('click',function(e){
+					layer.setStyle({fillColor: 'yellow', fillOpacity: 0.5});
+					$('#f11').html('SIGMET');
+					$('#f12').html(e.target.feature.properties.Alt);
+					$('#f13').html(e.target.feature.properties.RepNum);
+					$('#f14').html('n/a');
+					sigmet.stop();});
+	           layer.on('mouseout',function(e){
+					sigmet.start();})		
+			},
+				filter: function(feature,layer) {   
+					var n = document.getElementById("sgalt").value;
+					var nn = parseInt(n, 10);
+					return (feature.properties.Alt >= nn );
+				}
+		}).addTo(map);;  
 
 // ** METAR 
-	realtime = L.realtime({
-		url: 'http://localhost:8000/metar.geojson',
+var wxIcon = L.icon({iconUrl: 'therm.ico', iconSize: [35,25]});
+
+metar = L.realtime({
+	url: 'http://localhost:8000/metar.geojson',
 		crossOrigin: true, type: 'json'
-	}, {interval: 3 * 1000,
-  
-		getFeatureId: function(featureData) {
+	}, {interval: 123 * 1000,
+  		getFeatureId: function(featureData) {
 			return featureData.properties.Stn;
 		},
-
 		pointToLayer: function (feature, latlng) {
 			marker = L.marker(latlng,{icon: wxIcon});
-    		marker.bindTooltip('<b><u>'+feature.properties.Stn +'</b></u>' 
-    				+ '<br>Obs Date :' + '<b>' + feature.properties.ObDate  + '</b>'	
-    				+ '<br>Temp(f)  :' + '<b>' + feature.properties.Temp    + '</b>'    				
-    				+ '<br>Wind Sp  :' + '<b>' + feature.properties.WindSp  + '</b>'
-    				+ '<br>Wind Dir  :' + '<b>' + feature.properties.WindDir + '</b>'    				
-    				+ '<br>Altimeter  :' + '<b>' + feature.properties.Alt     + '</b>'
-    				+ '<br>Visibility  : ' + '<b>' + feature.properties.Vsby    + '</b>'
-    				+ '<br>Dew Pt(f)  :' + '<b>' + feature.properties.DewP    + '</b>'
-    		);
-  		
+    		marker.bindTooltip(feature.properties.Stn 
+    				+ '<br>' + feature.properties.Temp   +'&#x2109');
+ 			marker.on('click', function (e) {
+					$('#f1').html(e.target.feature.properties.Stn);
+					$('#f2').html(e.target.feature.properties.Loc);
+					$('#f3').html(e.target.feature.properties.ObDate);
+					$('#f4').html(e.target.feature.properties.Temp);
+					$('#f5').html(e.target.feature.properties.WindSp);  
+					$('#f6').html(e.target.feature.properties.Vsby);
+			});
     		marker.addTo(map);
     		return marker;
 		}
 	}).addTo(map);
-    
 
-//  realtime.on('update', function() {
-//  map.fitBounds(realtime.getBounds(), {maxZoom: 8});
-//  map.fitBounds(realtime.getBounds());
-//});	
+// Handles the check boxes being turned on/off
+document.querySelector("input[name=gmet]").addEventListener('change', function() {
+                if(this.checked) map.addLayer(gairmet)
+                  else map.removeLayer(gairmet)
+                })
+   
+document.querySelector("input[name=amet]").addEventListener('change', function() {
+                if(this.checked) map.addLayer(airmet)
+                  else map.removeLayer(airmet)
+                })
 
-/*
-	var myStyle = {
-			"color": "#ff7800",
-   		    "weight": 5,
-   		    "opacity": 0.65
-   		};
-*/	
-/*	
-	$.getJSON("geo2.txt",function(data){
-		function onEachFeature(feature,layer){
-			layer.bindPopup(feature.properties.MSG + '<br/>' + feature.properties.Location);
-		}
-		var moo = L.geoJson(data,{
-			onEachFeature: onEachFeature
-		}).addTo(map);
-	});     
-*/
+document.querySelector("input[name=meta]").addEventListener('change', function() {
+                if(this.checked) map.addLayer(metar)
+                  else map.removeLayer(metar)
+                })
+
+document.querySelector("input[name=smet]").addEventListener('change', function() {
+                if(this.checked) map.addLayer(sigmet)
+                  else map.removeLayer(sigmet)
+                })
+
+//Add layer control
+var baseMaps = {
+    "Open Street Map": osm,
+    "Imagery":Esri_WorldImagery,
+	"Gray":Esri_WorldGrayCanvas,
+	"OSM B&W":OpenStreetMap_BlackAndWhite
+};
+
+var overlayMaps = {};
+L.control.layers(baseMaps, overlayMaps).addTo(map);
