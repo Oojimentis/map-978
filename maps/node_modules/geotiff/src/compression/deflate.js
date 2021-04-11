b7c7b44e46255be93815963928957b8606ffdf6c
trev <Oojimentis>
2021-04-11 19:53:43 -0400

    Add node modules

---
+++ maps/node_modules/geotiff/src/compression/deflate.js
@@ -0,0 +1,8 @@
+import { inflate } from 'pako/lib/inflate';
+import BaseDecoder from './basedecoder';
+
+export default class DeflateDecoder extends BaseDecoder {
+  decodeBlock(buffer) {
+    return inflate(new Uint8Array(buffer)).buffer;
+  }
+}
