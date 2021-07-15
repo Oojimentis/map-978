d3cbfb46f42ed94c4cbe2a1d90a4218247855b89
trev <trev@trev>
2021-07-14 20:38:13 -0400

    TAF details wip

--- maps/public/map978p3.js
+++ maps/public/map978p3.js
@@ -2,14 +2,36 @@ var server_port = document.getElementById('port').value;
 var host_url = "http://localhost:";
 var url = host_url.concat(server_port, "/sqlx?q=");
 
-var stn_sql = url.concat("select stn_call,ob_date,temp,windsp from metar  &m=page3");
+var stn_sql = url.concat("select stn_call,current,wind,visby,condx,rep_time,issued from taf &m=page3");
+
+/* Function for child row details*/
+
+function getChildRow(data) {
+           
+// `data` is the data object for the row
+	return '<table cellpadding="5" cellspacing="0"'
+		+ ' style="padding-left:50px;">' +
+		'<tr>' +
+		'<td>Station:</td>' +
+		'<td>' + data.stn_call + '</td>' +
+		'</tr>' +
+		'<tr>' +
+		'<td>Report Time:</td>' +
+		'<td>' + data.rep_time + '</td>' +
+		'</tr>' +
+		'<tr>' +
+		'<td>Issued At:</td>' +
+		'<td>' + data.issued + '</td>' +
+		'</tr>' +
+		'</table>';
+}
 
 $(document).ready(function () {
 	$.ajax({
 		type: "get",
 		url: stn_sql,
 		success: function(result) {
-			$('#tableID').DataTable({
+			var table=	$('#tableID').DataTable({
 				data: result,
 				columns: [{
 					"className": 'details-control',
@@ -18,12 +40,30 @@ $(document).ready(function () {
 					"defaultContent": ''
 				},					
 					{ data: 'stn_call' },
-					{ data: 'ob_date' },
-					{ data: 'temp' },
-					{ data: 'windsp' }
+					{ data: 'current' },
+					{ data: 'wind' },
+					{ data: 'visby' },
+					{ data: 'condx' }
 				]
 			});
+			$('#tableID tbody').on('click','td.details-control', function () {
+				var tr = $(this).closest('tr');
+				var row = table.row(tr);
+				if (row.child.isShown()) {
+					// Closing the already opened row           
+					row.child.hide();
+					// Removing class to hide
+					tr.removeClass('shown');
+				}
+				else {
+					// Show the child row for detailinformation
+					row.child(getChildRow(row.data())).show();
+
+					// To show details,add the below class
+					tr.addClass('shown');
+				}
+			});			
 		}
-	});	
+	});
 });
 	
