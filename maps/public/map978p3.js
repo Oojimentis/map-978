1fad06b14d45eb7ae6a03767ef1cf55040fbe0e5
trev <Oojimentis>
2021-07-16 13:09:51 -0400

    TAF details styling.

--- maps/public/map978p3.js
+++ maps/public/map978p3.js
@@ -2,36 +2,39 @@ var server_port = document.getElementById('port').value;
 var host_url = "http://localhost:";
 var url = host_url.concat(server_port, "/sqlx?q=");
 
-var stn_sql = url.concat("select stn_call, current, wind, visby, condx, rep_time, issued from taf &m=page3");
+var stn_sql = url.concat("SELECT stn_call, current, wind, visby, condx, rep_time, issued \
+					from taf &m=page3");
 var thead = '';
 var tbody = '';
 
 function getChildRow(callback,data) {
 
-	var sub ='select stn_call as "Station",rep_time as "Time",forecast as "Forecast" from taf_forecast \
-		where stn_call = \'' + data.stn_call + '\' and \
-		rep_time =\''+ data.rep_time+ '\' &m=subsql';
+	var sub ='SELECT stn_call AS "Station", rep_time AS "Time", forecast AS "Forecast" \
+				FROM taf_forecast \
+				WHERE stn_call = \'' + data.stn_call + '\' AND \
+				rep_time =\''+ data.rep_time+ '\' &m=TAF child rows';
 	var sub_sql = url.concat(sub);
-		$.ajax({  
-			type: "Get",
-			url: sub_sql,
-			success: function(sdata) {
-				var thead = ''; 
-		 		var tbody = '';
-				for (var key in sdata[0]) {
-					thead += '<th>' + key + '</th>';
-				}
-				$.each(sdata, function (i, d) {
-					tbody += '<tr><td>' + d.Station
-						+ '</td><td>' + d.Time
-						+ '</td><td>' + d.Forecast
-						+ '</td></tr>';
-				});
 
-				callback($('<table cellpadding="5" cellspacing="0"'
-					+ ' style="padding-left:50px;">' + thead + tbody + '</table>')).show();
+	$.ajax({  
+		type: "Get",
+		url: sub_sql,
+		success: function(sdata) {
+			var thead = ''; 
+	 		var tbody = '';
+			for (var key in sdata[0]) {
+				thead += '<th>' + key + '</th>';
 			}
-		});
+			$.each(sdata, function (i, d) {
+				tbody += '<tr><td>' + d.Station
+					+ '</td><td>' + d.Time
+					+ '</td><td>' + d.Forecast
+					+ '</td></tr>';
+			});
+
+			callback($('<table class="taf_child"'
+				+ ' style="padding-left:50px;">' + thead + tbody + '</table>')).show();
+		}
+	});
 }
 
 $(document).ready(function () {
@@ -65,8 +68,7 @@ $(document).ready(function () {
 				}
 				else {
 					// Show the child row for detailinformation
-//					row.child(getChildRow(row.data())).show();
-				getChildRow(row.child,row.data());
+					getChildRow(row.child,row.data());
 					// To show details,add the below class
 					tr.addClass('shown');
 				}
