6297d540ba88606a0354ebc1414a49f233f284d3
Oojimentis <60933475+Oojimentis@users.noreply.github.com>
2021-07-17 14:21:55 -0400

    Delete sua-check.sql

--- maps/sua-check.sql
+++
@@ -1,5 +0,0 @@
-select distinct s.airsp_id, s.airsp_name,a.airsp_id, a.airsp_name,s.airsp_type
-from sua s
-left join sua_airspace a on a.airsp_id = s.airsp_id 
-where a.airsp_name IS NULL
-order by s.airsp_name
