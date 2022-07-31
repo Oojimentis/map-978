d36b3aa96416768cccb51bddb7e8c0f5381a1c84
trev <Oojimentis>
2022-07-30 21:40:10 -0400

    METAR details screen

--- maps/public/map978p7.js
+++ maps/public/map978p7.js
@@ -4,27 +4,28 @@ var server_port = document.getElementById('port').value;
 var host_url = "http://localhost:";
 var url = host_url.concat(server_port, "/sqlx?q=");
 
-var stn_sql = url.concat("SELECT t.stn_call, CONCAT(s.stn_loc,', ', s.state) AS stn_loc, \
-	ob_date, \
-	CONCAT('Tp: ', temperature, '°F <BR>Dp: ', dewp,'°F') AS temperature, \
-	CONCAT( \
-		(CASE WHEN windsp <> '-' THEN CONCAT('Sp: ', windsp, ' kts<br>') \
-		 ELSE windsp END), \
-		(CASE WHEN winddir = 'Variable' THEN concat('Dir: ', winddir, '<BR>') \
-			  WHEN winddir <> '-' THEN concat('Dir: ', winddir, 'deg<BR>') \
-	  		  ELSE winddir END), \
-		(CASE WHEN windgust <> '-' THEN CONCAT('Gust: ', windgust, ' kts') \
-		 ELSE windgust END)) AS windsp, \
-	CONCAT(altimeter, ' Ins<BR>', slp,' hPa') AS slp, \
-		CASE WHEN visby <> '-' THEN concat(visby,' sm') \
-		ELSE visby END AS visby, \
-	hrly_precip, mtype, \
-	CONCAT(cld_type1, '<BR>', cld_type2,'<BR>' , cld_type3,'<BR>', \
-		   cld_type4, '<BR>', cld_type5) AS cld_type1, \
-	wx_obstruct FROM postgis.metar t \
-	INNER JOIN stations s ON s.stn_call = t.stn_call \
-	ORDER BY t.stn_call \
-	&m=METAR Details");
+var stn_sql = url.concat("SELECT t.stn_call, CONCAT(s.stn_loc,', ', s.state) AS stn_loc, ob_date, \
+CONCAT( \
+	CASE WHEN temperature <> '- ' THEN CONCAT('Tp: ', temperature, '°F <BR>') ELSE temperature END, \
+	CASE WHEN dewp <> '-' THEN CONCAT('Dp: ', dewp,'°F') ELSE dewp END) AS temperature, \
+CONCAT( \
+	(CASE WHEN windsp <> '-' THEN CONCAT('Spd: ', windsp, ' kts<BR>') ELSE windsp END), \
+	(CASE WHEN winddir = 'Variable' THEN CONCAT('Dir: ', winddir, '<BR>') \
+		  WHEN winddir <> '-' THEN CONCAT('Dir: ', winddir, 'deg<BR>') ELSE winddir END), \
+	(CASE WHEN windgust <> '-' THEN CONCAT('Gust: ', windgust, ' kts') ELSE windgust END)) AS windsp, \
+CONCAT( \
+	CASE WHEN altimeter <> '-' THEN CONCAT('Alt: ',altimeter, ' Ins<BR>') ELSE altimeter END, \
+	CASE WHEN slp <> '-' THEN CONCAT('SLP: ',slp,' hPa') ELSE slp END) AS slp, \
+CONCAT( \
+	CASE WHEN visby <> '-' THEN CONCAT('Vis: ',visby,' sm<BR>') ELSE visby END ,\
+   (CASE WHEN hrly_precip <> '- ' THEN CONCAT('Precip: ',hrly_precip,'in/hr<BR>') \
+	ELSE CONCAT(hrly_precip,'<BR>') END), wx_obstruct) AS wx_obstruct, \
+	mtype, \
+CONCAT(cld_type1, '<BR>', cld_type2,'<BR>', cld_type3, '<BR>', cld_type4, '<BR>', cld_type5) AS cld_type1 \
+FROM postgis.metar t \
+INNER JOIN stations s ON s.stn_call = t.stn_call \
+ORDER BY t.stn_call \
+&m=METAR Details");
 
 $(document).ready(function() {
 	$.ajax({
@@ -38,15 +39,13 @@ $(document).ready(function() {
 				lengthMenu: [10, 25, 50, 100, 250, 500],
 				columns: [ 
 					{ "width": "1px", data: 'stn_call'},
-					{ "width": "45px", data: 'stn_loc' },
+					{ "width": "95px", data: 'stn_loc' },
 					{ "width": "42px",data: 'ob_date' },									
 					{ "width": "5px", data: 'mtype' },
 					{ "width": "1px", data: 'temperature' },
-					{ data: 'windsp' },
-					{ data: 'slp' },					
-					{ data: 'visby'},
-					{ "width": "5px",data: 'hrly_precip' },									
-					{ data: 'wx_obstruct' },
+					{ "width": "70px",data: 'windsp' },
+					{ "width": "90px",data: 'slp' },					
+					{ "width" : "80px",data: 'wx_obstruct' },
 					{ data: 'cld_type1'}
 				]
 			});
